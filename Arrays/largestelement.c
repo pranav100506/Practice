f2f77d13e407354930a7ec8e072f6b2d3e6c5dd8
Pranav Somashekar <pranavsomashekar1@gmail.com>
2026-02-03 15:15:14 +0530

    updated array input code

--- Arrays/largestelement.c
+++ Arrays/largestelement.c
@@ -1,1 +1,23 @@
 #include<stdio.h>
+#include<stdbool.h>
+
+int largest_element(int a[],int size){
+    int largest = -1;
+    for(int i =0;i<size;i++){
+        if(a[i]>largest){
+            largest =a[i];
+        }
+    }
+    return largest;
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int x = largest_element(arr,n);
+    printf("%d",x);
+    return 0;
+}
